ccde969c2b4224864a0c5fdb864038f40a36868d
Maxim Keegan <keegan@tochka.com>
2023-10-16 17:16:10 +0500

    Initial Commit

---
+++ SlenderRow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SlenderRow
+//
+//  Created by Maxim Keegan on 16.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SlenderRow/SlenderRowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlenderRowApp.swift
+//  SlenderRow
+//
+//  Created by Maxim Keegan on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SlenderRowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
